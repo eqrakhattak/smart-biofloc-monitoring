ebed01458eebe1c06e974b18511b4ffbccdb3cd3
eqrakhattak <eqra.khattak7@gmail.com>
2022-05-19 03:16:33 +0500

    Create Fish Page

--- sci_fish/lib/pages/fish_page.dart
+++ sci_fish/lib/pages/fish_page.dart
@@ -1,10 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:sci_fish/constants.dart';
 
 class FishPage extends StatelessWidget {
   const FishPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello Fish');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fish'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 5.0,
+              ),
+              ListTile(
+                title: const Text(
+                  'Alive Fish',
+                  style: TextStyle(
+                    color: textColor,
+                  ),
+                ),
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Color(0xFF10898d), width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                //TODO: Change tap color to teal
+                //TODO: Add OnTap functionality
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              ListTile(
+                title: const Text(
+                  'Dead Fish',
+                  style: TextStyle(
+                    color: textColor,
+                  ),
+                ),
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Color(0xFF10898d), width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              const SizedBox(
+                height: 55.0,
+              ),
+              const Text(
+                  'Mortality Rate:',
+                style: TextStyle(
+                  fontSize: 17.0,
+                  color: textColor,
+                ),
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              Expanded(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.white12,
+                    child: const Center(
+                      child: Text(
+                        'The Temperature Graph HERE',
+                        style: TextStyle(
+                          color: textColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
