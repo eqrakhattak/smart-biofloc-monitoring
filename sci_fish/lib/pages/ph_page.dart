4d38b823d2ba2dfbb7454739075f905f9deb5c9c
eqrakhattak <eqra.khattak7@gmail.com>
2022-05-21 19:01:37 +0500

    Create pH Page

--- sci_fish/lib/pages/ph_page.dart
+++ sci_fish/lib/pages/ph_page.dart
@@ -1,10 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:sci_fish/constants.dart';
 
 class PhPage extends StatelessWidget {
   const PhPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello Ph');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('pH'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 5.0,
+              ),
+              Material(
+                color: Colors.cyan[300],
+                elevation: 4,
+                borderRadius: BorderRadius.circular(10),
+                shadowColor: Colors.blue,
+                child: ListTile(
+                  title: const Text(
+                    'pH Sensor ON/OFF',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  trailing: const Icon(Icons.power_settings_new),
+                  iconColor: colorOff,
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: Color(0xFF10898d), width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  onTap: () => print('pH Off'),
+                  //TODO: Add OnTap functionality
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 5.0, top: 5.0),
+                child: Text(
+                  'pH Sensor OFF',
+                  style: TextStyle(
+                    color: colorOff,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 1,
+                child: ListTile(
+                  title: const Text(
+                    'Pot Pin Value',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  subtitle: const Text(
+                    '278.00',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 33.0,
+                      //TODO: edit textsize according to expanded
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: Color(0xFF10898d), width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Expanded(
+                flex: 1,
+                child: ListTile(
+                  title: const Text(
+                    'pH',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  subtitle: const Text(
+                    '0.74',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 33.0,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: Color(0xFF10898d), width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              const Text(
+                'pH per Second:',
+                style: TextStyle(
+                  fontSize: 17.0,
+                  color: textColor,
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Expanded(
+                flex: 3,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.white12,
+                    child: const Center(
+                      //TODO: Add graph in here
+                      child: Text(
+                        'pH Graph HERE',
+                        style: TextStyle(
+                          color: textColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
