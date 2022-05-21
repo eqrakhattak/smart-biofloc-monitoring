370fdddea2191c7e406027a0706b42afa7f3fe64
eqrakhattak <eqra.khattak7@gmail.com>
2022-05-21 18:44:39 +0500

    Create Temprature Page

--- sci_fish/lib/pages/temp_page.dart
+++ sci_fish/lib/pages/temp_page.dart
@@ -1,10 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:sci_fish/constants.dart';
 
 class TemperaturePage extends StatelessWidget {
   const TemperaturePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text('hello temp');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Temperature'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 5.0,
+              ),
+              Material(
+                color: Colors.cyan[300],
+                elevation: 4,
+                borderRadius: BorderRadius.circular(10),
+                shadowColor: Colors.blue,
+                child: ListTile(
+                  title: const Text(
+                    'Air Pump ON/OFF',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  trailing: const Icon(Icons.power_settings_new),
+                  iconColor: colorOff,
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: Color(0xFF10898d), width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  onTap: () => print('Air Pump Off'),
+                  //TODO: Add OnTap functionality
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 5.0, top: 5.0),
+                child: Text(
+                  'Pump is OFF',
+                  style: TextStyle(
+                    color: colorOff,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 1,
+                child: ListTile(
+                  title: const Text(
+                    'Celsius Temperature',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  subtitle: const Text(
+                    '33°C',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 33.0,
+                      //TODO: edit textsize according to expanded
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: Color(0xFF10898d), width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Expanded(
+                flex: 1,
+                child: ListTile(
+                  title: const Text(
+                    'Fahrenheit Temperature',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  subtitle: const Text(
+                    '78°F',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 33.0,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: Color(0xFF10898d), width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              const Text(
+                'Temperature per Second:',
+                style: TextStyle(
+                  fontSize: 17.0,
+                  color: textColor,
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Expanded(
+                flex: 3,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.white12,
+                    child: const Center(
+                      //TODO: Add graph in here
+                      child: Text(
+                        'Temperature Graph HERE',
+                        style: TextStyle(
+                          color: textColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
