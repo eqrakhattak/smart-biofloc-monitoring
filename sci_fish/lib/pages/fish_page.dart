72b4ffd8716d38937ba0976109c9412252181c65
eqrakhattak <eqra.khattak7@gmail.com>
2022-05-22 17:43:33 +0500

    Upgrade Fish Page

--- sci_fish/lib/pages/fish_page.dart
+++ sci_fish/lib/pages/fish_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:sci_fish/constants.dart';
 
-class FishPage extends StatelessWidget {
+class FishPage extends StatefulWidget {
   const FishPage({Key? key}) : super(key: key);
 
   static const String id = 'fish_page';
+
+  @override
+  State<FishPage> createState() => _FishPageState();
+}
+
+class _FishPageState extends State<FishPage> {
+
+  // TextEditingController nameController = TextEditingController();
+  late String noOfAliveFish;
+  late String noOfDeadFish;
 
   @override
   Widget build(BuildContext context) {
@@ -12,91 +22,116 @@ class FishPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Fish Life'),
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 5.0,
-              ),
-              Material(
-                color: Colors.cyan[300],
-                elevation: 4,
-                borderRadius: BorderRadius.circular(10),
-                shadowColor: Colors.blue,
-                child: ListTile(
-                  title: const Text(
-                    'Alive Fish',
-                    style: TextStyle(
+      body: GestureDetector(
+        //loose focus when clicked outside the text fields and hide keyboard
+        onTapDown: (_) => FocusManager.instance.primaryFocus?.unfocus(),
+        behavior: HitTestBehavior.translucent,
+
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 10.0,
+                ),
+                TextField(
+                  // controller: nameController,
+                  keyboardType: TextInputType.number,
+                  maxLength: 2,
+                  style: const TextStyle(
+                    color: textColor,
+                  ),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Color(0xFF10898d),),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    labelText: 'Alive Fish',
+                    labelStyle: const TextStyle(
+                      fontSize: 22.0,
                       color: textColor,
                     ),
                   ),
-                  //TODO: add textfield to enter the number of fishes dead or alive
-                  shape: RoundedRectangleBorder(
-                    side: const BorderSide(color: Color(0xFF10898d), width: 2),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  //TODO: Add OnTap functionality
+                  onChanged: (value) {
+                    noOfAliveFish = value;
+                    print(noOfAliveFish);
+                  },
                 ),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              Material(
-                color: Colors.cyan[300],
-                elevation: 4,
-                borderRadius: BorderRadius.circular(10),
-                shadowColor: Colors.blue,
-                child: ListTile(
-                  title: const Text(
-                    'Dead Fish',
-                    style: TextStyle(
+                const SizedBox(
+                  height: 10.0,
+                ),
+                TextField(
+                  // controller: nameController,
+                  keyboardType: TextInputType.number,
+                  maxLength: 2,
+                  style: const TextStyle(
+                    color: textColor,
+                  ),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    labelText: 'Dead Fish',
+                    labelStyle: const TextStyle(
+                      fontSize: 22.0,
                       color: textColor,
                     ),
                   ),
-                  shape: RoundedRectangleBorder(
-                    side: const BorderSide(color: Color(0xFF10898d), width: 2),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                  onChanged: (value) {
+                    noOfDeadFish = value;
+                    print(noOfDeadFish);
+                  },
                 ),
-              ),
-              const SizedBox(
-                height: 55.0,
-              ),
-              const Text(
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
                   'Mortality Rate:',
-                style: TextStyle(
-                  fontSize: 17.0,
-                  color: textColor,
+                  style: TextStyle(
+                    fontSize: 17.0,
+                    color: textColor,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 15.0,
-              ),
-              Expanded(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: Container(
-                    width: double.infinity,
-                    color: Colors.white12,
-                    child: const Center(
-                      //TODO: Add graph in here
-                      child: Text(
-                        'The Mortality Graph HERE',
-                        style: TextStyle(
-                          color: textColor,
+                const SizedBox(
+                  height: 15.0,
+                ),
+                Expanded(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Container(
+                      width: double.infinity,
+                      color: Colors.white12,
+                      child: const Center(
+                        //TODO: Add graph in here
+                        child: Text(
+                          'The Mortality Graph HERE',
+                          style: TextStyle(
+                            color: textColor,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+//
+// class FishPage extends StatelessWidget {
+//   const FishPage({Key? key}) : super(key: key);
+//
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return
+//   }
+// }
