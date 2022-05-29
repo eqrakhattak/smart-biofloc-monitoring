7385934e51a8bc83c668d1ebeed3b3dd3fe9a651
eqrakhattak <eqra.khattak7@gmail.com>
2022-05-30 01:31:43 +0500

    Add Water Level Sensor

--- Arduino/sensors_code/sensors_code.ino
+++ Arduino/sensors_code/sensors_code.ino
@@ -1,44 +1,52 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-// GPIO where the DS18B20 is connected to temp sensor
-const int oneWireBus = 5;     
-// Setup a oneWire instance to communicate with any OneWire devices
-OneWire oneWire(oneWireBus);
-// Pass our oneWire reference to Dallas Temperature sensor 
+ 
+// GPIO Data wire is connected to D2 & 3V & G on the esp8266
+#define ONE_WIRE_BUS 4
+int Sensor = 5;  //RCWL-0516 Input Pin
+int led = 2; //LED Pin
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);
+// Pass oneWire reference to DallasTemperature library
 DallasTemperature sensors(&oneWire);
 
-
-// pin connected to ph sensor
+// A0 pin connected to ph sensor & 3V & G
 const int potPin=A0;
 float ph;
 float Value=0;
 
-
-void setup() {
-  //blink
+void setup(void)
+{
+  //blink builtin led
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   
-  //temp sensor code:
-  // Start the Serial Monitor
-  Serial.begin(115200);
-  // Start the DS18B20 sensor
+  // Start up the DS18B20 temperature sensor
   sensors.begin();
+  //Start serial monitor
+  Serial.begin(115200);
+
+  // water level sensor connected to D1 & 3V
+  pinMode(Sensor,INPUT);
+  //led connected to D4 & G
+  pinMode(led, OUTPUT);
 
   // put your ph sensor setup code:
   pinMode(potPin,INPUT);
+
+  //delay in readings 1sec
   delay(1000);
 }
-
-void loop() {
-  //blinks over n over
+ 
+void loop(void)
+{ 
+  //Esp8266 builtin led blinks over n over
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);   
-  
-  //temp code:
+  delay(1000); 
+ 
+  //print the temperature in Celsius and Fehrenheit
   sensors.requestTemperatures(); 
   float temperatureC = sensors.getTempCByIndex(0);
   float temperatureF = sensors.getTempFByIndex(0);
@@ -47,13 +55,26 @@ void loop() {
   Serial.print(temperatureF);
   Serial.println("ºF");
 
-  //ph code:
+  //print pH
   Value= analogRead(potPin);
   Serial.print(Value);
   Serial.print(" | ");
   float voltage=Value*(3.3/4095.0);
   ph=(3.3*voltage);
   Serial.println(ph);
-  
-  delay(5000);
+
+  //water level code
+   if (digitalRead(Sensor) == HIGH )
+      {
+        digitalWrite(led,HIGH);
+        Serial.flush();
+        Serial.println( "level full" ); 
+      }
+      else {
+        digitalWrite(led, LOW);
+        Serial.println( "level remaining" ); 
+      } 
+      
+   delay(1000);
+
 }
